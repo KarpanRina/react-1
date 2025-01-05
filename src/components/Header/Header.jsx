559d54a0d8e5b8226658aacaa62b87ec02597b9f
Rina <arias.workplace@example.com>
2025-01-05 18:45:49 +0200

    1st

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import './Header.css';
+import LogoYummy from './../../assets/LogoYummy.svg';
+
+function Header() {
+    return (
+        <header>
+        <div className="container">
+            <nav>
+                <a href="/" className="logo">
+                    <img src={LogoYummy} alt="Logo" />
+                </a>
+                <div className="menu">
+                    <a href="#">Порада від Ямі</a>
+                    <a href="#">Мої улюбленній солодощі</a>
+                </div>
+            </nav>
+        </div>
+        <div className="container header-container">
+            <div className="greetings">
+                <h1><span>Yummy</span> <br /> Pug</h1>
+                <p>
+                    Всім привіт, я Ямі і я мопс, який дуже сильно полюбляє солодощі. Сьогодні я розкажу вам, чому саме
+                    солодощі і чому вони так важливі в нашому житті.Доречі, моє імʼя з англійської мови перекладається
+                    як “Смачний”.
+                </p>
+                <a href="#">Мої улюбленні солодощі</a>
+            </div>
+        </div>
+        </header>
+    );
+  }
+  
+  export default Header;
